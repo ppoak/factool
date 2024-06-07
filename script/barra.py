e46ee44431f4b4f2b06f431eb45f2fde9db1f8fc
wiikai <whua5016@gmail.com>
2024-06-07 11:21:37 +0800

    Fix Barra Return

--- script/barra.py
+++ script/barra.py
@@ -6,88 +6,78 @@ import factorlab as lab
 import dataforge as forge
 from tqdm import  tqdm
 from joblib import Parallel, delayed
-
+from sklearn.preprocessing import StandardScaler
 
 class BarraReturn(quool.DatetimeTable, lab.Factor):
 
     def get_barra_return(self, date: str):
+        # r = f * X 
+        # where f = w * r
+        # r = (w * r) * X
+        # constraint：when ∑(w(i) * X) = 1 ， ∑(w(other) * X) = 0
 
         rollback = lab.quotes_day.get_trading_days_rollback(date, 1)
         price = lab.quotes_day.read('close', start=rollback, stop=date)
         _adj = lab.quotes_day.read('adjfactor', start=rollback, stop=date)
         shares = lab.quotes_day.read("circulation_a", start=date, stop=date)
-
-        # 个股市值权重
         size = (shares * price * _adj).loc[date]
-        weight = size.apply(lambda x: x / size.sum())
-        weight.name = 'weight'
 
-        # 回归权重矩阵
-        reg_weight = size.apply(lambda x: np.sqrt(x) / np.sqrt(size.sum()))
-        reg_weight.name = 'reg_weight'
-        # V = np.diag(reg_weight)
-
-        # 不考虑rf的超额收益
-        ret = (price * _adj).pct_change(fill_method=None).loc[date]
-        ret.name = 'ret'
-
+        # 行业因子
         ind = forge.industry_info.read('first_industry_name',start=date, stop=date)
         ind = ind.reset_index(level='date', drop=True)
         ind = pd.get_dummies(ind, prefix='', prefix_sep='')
         ind = ind.select_dtypes(include=[bool]).astype(int) 
-        industry_columns = ind.columns # 获取行业名字
-        ind['国家'] = 1
 
-        # 因子数据
-        df = lab.factor.read(
+        # 风格因子
+        style = lab.factor.read(
             'log_marketcap, market_beta, nonrecent_momentum, residual_volatility,'
             'nonlinear_size, book_to_price, compound_turnover, compound_leverage',
             start=rollback, stop=rollback
         )
-        df = df.reset_index(level='date', drop=True)
-        df = df.apply(lambda x: (x - np.sum(x * weight))/ x.std()) # 因子截面标准化
-        df = pd.concat([df,ind,ret], axis=1).dropna()
+        style = style.reset_index(level='date', drop=True)
+        scaler = StandardScaler()
+        scaled_style = pd.DataFrame(scaler.fit_transform(style), index=style.index, columns=style.columns)
 
-        # 匹配长度
-        industry_columns = industry_columns.intersection(df.columns)
-        reg_weight = reg_weight.loc[df.index]
-        
-        # 计算因子收益
-        X = df.drop(['ret'], axis=1)
-        y = df['ret']
+         # 国家因子
+        country = pd.Series(np.ones(len(style.index)), index=style.index, name='country').to_frame()
+        X = pd.concat([country, ind, scaled_style], axis=1).dropna()
 
-        # 设置变量
+        # 回归权重矩阵
+        reg_weight = np.sqrt(size) / np.sqrt(size).sum()
+        reg_weight = reg_weight.reindex(X.index)
+        reg_weight.index.name = ''
+        V = pd.DataFrame(np.diag(reg_weight), index=reg_weight.index, columns=reg_weight.index)
+
+        # 约束矩阵 K*K-1
         N, K = X.shape
-        f = cp.Variable(K)
-        w = cp.Variable((K, N))
-        residuals = y.values - X.values @ f
+        R = np.diag(np.ones(K))
+        R = np.delete(R, len(ind.columns), axis=1)
 
-        # 加权最小化残差平方和
-        weighted_residuals = cp.multiply(reg_weight, residuals)
-        objective = cp.Minimize(cp.sum_squares(weighted_residuals))
+        ind_weight = ind.mul(reg_weight, axis=0).sum()
+        ind_weight_adj = -ind_weight.div(ind_weight.iloc[-1]).iloc[:-1]
+        R[len(ind.columns),1:len(ind_weight_adj)+1] = ind_weight_adj.values
+        R = pd.DataFrame(R, index=X.columns, columns=X.columns.drop(ind.columns[-1]))
 
-        # r = f * X 
-        # where f = w * r
-        # r = (w * r) * X
-        # constraint：when ∑(w(i) * X) = 1 ， ∑(w(other) * X) = 0
-        # 添加约束条件
-        industry_constraints = 0
-        for col in industry_columns:
-            industry_weight = (weight * df[col]).sum()
-            industry_index = X.columns.get_loc(col)
-            industry_constraints += f[industry_index] * industry_weight
+        # 带约束，且考虑异方差的股票权重矩阵 omega = R @ (R.T @ X.T @ V @ X @ R)^-1 @ R.T @ X.T @ V
+        W = np.dot(R.dot(np.linalg.inv(R.T.dot(X.T).dot(V).dot(X).dot(R))),R.T).dot(X.T).dot(V)
+        W = pd.DataFrame(W, index=X.columns, columns=X.index)
 
-        f_constraint = [f == w @ y.values]
+        # 无约束，但考虑异方差的股票权重矩阵，可以直接WLS。omega = (X.T @ V @ X)^-1 @ X.T @ V
+        # W = np.linalg.inv(X.T.dot(V).dot(X)).dot(X.T).dot(V)
+        # W = pd.DataFrame(W, index=X.columns, columns=X.index)
 
-        exposure_constraints = [(w @ X.values)== np.eye(K)]
+        # 无约束，无异方差的股票权重矩阵，可以直接OLS。omega = (X.T @ X)^-1 @ X.T
+        # W = np.linalg.inv(X.T.dot(X)).dot(X.T)
+        # W = pd.DataFrame(W, index=X.columns, columns=X.index)
+        
+        # 因子收益率, r=不考虑rf的超额收益
+        r = (price * _adj).pct_change(fill_method=None).loc[date]
+        r = r.reindex(X.index)
+        r.name = 'ret'
 
-        problem = cp.Problem(objective, [industry_constraints == 0] + f_constraint + exposure_constraints)
-        problem.solve()
-
-        # 获取回归系数
-        res = pd.Series(f.value, index=df.drop(['ret'], axis=1).columns)
-        res.name = date
-        return res   
+        f = W.dot(r)
+        f.name = date
+        return f   
 
 barrareturn = BarraReturn("./data/barra-returns")
 
