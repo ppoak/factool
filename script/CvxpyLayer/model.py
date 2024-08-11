d6481cd9afb13deeb5e06c933d0e1918a345e3f9
wiikai <whua5016@gmail.com>
2024-08-11 16:35:11 +0800

    rebuild cvxpy model

--- script/CvxpyLayer/model.py
+++ script/CvxpyLayer/model.py
@@ -36,37 +36,40 @@ class RiskBudgetModel(nn.Module):
         )
 
     def forward(self, x, Q_sqrt):
-        # # x 的形状应为 [batch_size, 5, 11]
-        # b = self.fc1(x.view(x.size(0), -1)) # [batch_size, 5*11]
-        # b = self.leaky_relu(b)
-        # b = self.fc2(b)
-        # b = self.softmax(b)
-        # b = self.hardtanh(b)
-        # b = minmax(b)
-
-        # y, = self.cvxpy_layer(b, Q_sqrt)
-        # w = y / y.sum()
-        # return w
-    
-        b = self.fc1(x)
+        # x 的形状应为 [batch_size, 5, 11]
+        b = self.fc1(x.view(x.size(0), -1)) # [batch_size, 5*11]
         b = self.leaky_relu(b)
         b = self.fc2(b)
         b = self.softmax(b)
         b = self.hardtanh(b)
         b = minmax(b)
-        b = b.view(b.size(0), -1)
 
         y, = self.cvxpy_layer(b, Q_sqrt)
-        w = y / y.sum()
+        w = y / y.sum(dim=1, keepdim=True)
         return w
     
-def train_model(dataloader, model, optimizer, epochs=50, early_stopping=10):
+        # b = self.fc1(x)
+        # b = self.leaky_relu(b)
+        # b = self.fc2(b)
+        # b = self.softmax(b)
+        # b = self.hardtanh(b)
+        # b = minmax(b)
+        # b = b.view(b.size(0), -1)
+
+        # y, = self.cvxpy_layer(b, Q_sqrt)
+        # w = y / y.sum(dim=1, keepdim=True)
+        # return w
+    
+def train_model(train_dataloader,test_dataloader, model, optimizer, epochs=50, early_stopping=10):
     patience_counter = 0
     best_loss = np.inf
+    train_losses = []
+    test_losses = []
 
     for epoch in range(epochs):
+        model.train()
         epoch_loss = 0.0
-        for idx, (batch_features, batch_Q_sqrt, batch_labels) in enumerate(dataloader):
+        for idx, (batch_features, batch_Q_sqrt, batch_labels) in enumerate(train_dataloader):
             optimizer.zero_grad()
             weights = model(batch_features, batch_Q_sqrt)
             ret = torch.mul(weights, batch_labels)
@@ -78,11 +81,17 @@ def train_model(dataloader, model, optimizer, epochs=50, early_stopping=10):
             # if idx % 5 == 0:
             #     print('Current epoch: %d, Current batch: %d, Loss is %.3f' %(epoch+1,idx+1,loss.item()))
 
-        epoch_loss /= len(dataloader)
-        print(f'Epoch {epoch+1}/{epochs}, Loss: {epoch_loss}')
+        epoch_loss /= len(train_dataloader)
+        train_losses.append(epoch_loss)
 
-        if epoch_loss < best_loss:
-            best_loss = epoch_loss
+        model.eval()
+        test_loss = test_model(test_dataloader, model)
+        test_losses.append(test_loss)
+
+        print(f'Epoch {epoch+1}/{epochs}, Train Loss: {epoch_loss}, Test Loss: {test_loss}')
+
+        if test_loss < best_loss:
+            best_loss = test_loss
             patience_counter = 0
             torch.save(model.state_dict(), 'FactorModel.pth')
         else:
@@ -91,3 +100,15 @@ def train_model(dataloader, model, optimizer, epochs=50, early_stopping=10):
         if patience_counter >= early_stopping:
             print("Early stopping")
             break
+
+    return train_losses, test_losses
+
+def test_model(dataloader, model):
+    total_loss = 0.0
+    with torch.no_grad():
+        for idx, (batch_features, batch_Q_sqrt, batch_labels) in enumerate(dataloader):
+            weights = model(batch_features, batch_Q_sqrt)
+            ret = torch.mul(weights, batch_labels)
+            loss = -torch.sum(ret)
+            total_loss += loss.item()
+    return total_loss / len(dataloader)
