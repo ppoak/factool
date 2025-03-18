a7d2426e71b0ecbacac0a6dea97117575731a53d
ppoak <ppoak@foxmail.com>
2025-03-18 13:29:33 +0800

    Add save method to FactorSource

--- factorlab/source.py
+++ factorlab/source.py
@@ -2,8 +2,10 @@ import duckdb
 import pandas as pd
 from pathlib import Path
 from xtquant import xtdata
+from functools import partial
 from quool import ParquetManager
 from abc import ABC, abstractmethod
+from .operators import zscore, madoutlier
 
 
 class FactorSource(ABC):
@@ -18,6 +20,10 @@ class FactorSource(ABC):
 
     @abstractmethod
     def get_factor(self, name: str, begin: str, end: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(self, name: str, df: pd.DataFrame):
         raise NotImplementedError
 
 
@@ -58,45 +64,44 @@ class XtFactorSource(FactorSource):
         factor.index = pd.to_datetime(factor.index)
         return factor
 
+    def save(self, name: str, df: pd.DataFrame):
+        raise ValueError("XtFactorSource does not support save operation")
 
-class ParquetFactorSource(FactorSource, ParquetManager):
+
+class ParquetFactorSource(FactorSource):
 
     def __init__(
         self,
         path: str | Path,
-        name_col: str = "name",
-        val_col: str = "value",
-        date_col: str = "date",
+        time_col: str = "time",
         code_col: str = "code",
-        partition: str = None,
     ):
-        ParquetManager.__init__(
-            self, path=path, index=[date_col, code_col, name_col], partition=partition
-        )
-        if bool(name_col) != bool(val_col):
-            raise ValueError(
-                "Either both name_col and val_col are provided or neither is provided"
-            )
-        self.date_col = date_col
+        self.path = Path(path)
+        self.time_col = time_col
         self.code_col = code_col
-        self.name_col = name_col
-        self.val_col = val_col
+
+    @property
+    def names(self):
+        return list(sorted(self.path.iterdir()))
 
     def get_times(
         self,
         begin: str | pd.Timestamp = None,
         end: str | pd.Timestamp = None,
-        persistence_code: str = "000001.XSHG",
+        code: str = "000001.XSHG",
     ):
+        if self.names:
+            name = self.names[0]
+        pm = ParquetManager(self.path / name)
         params = {
             "index": self.date_col,
-            self.code_col: persistence_code,
+            self.code_col: code,
         }
         if begin is not None:
             params[f"{self.date_col}__ge"] = pd.to_datetime(begin)
         if end is not None:
             params[f"{self.date_col}__le"] = pd.to_datetime(end)
-        return ParquetManager.read(self, **params).index.unique().sort_values()
+        return pm.read(**params).index.unique().sort_values()
 
     def get_time(self, time: str | pd.Timestamp = None, n: int = 1):
         time = pd.to_datetime(time or "now")
@@ -114,28 +119,57 @@ class ParquetFactorSource(FactorSource, ParquetManager):
         name: str,
         begin: pd.Timestamp | str = None,
         end: pd.Timestamp | str = None,
+        processed: bool = False,
         **kwargs,
     ) -> pd.DataFrame:
-        if self.name_col:
-            kwargs.update(
-                {
-                    "pivot": self.val_col,
-                    self.name_col: name,
-                    "index": self.date_col,
-                    "columns": self.code_col,
-                }
-            )
-        else:
-            kwargs.update(
-                {"pivot": name, "index": self.date_col, "columns": self.code_col}
-            )
+        pm = ParquetManager(self.path / name)
+        kwargs.update(
+            {
+                "pivot": name + ("" if processed else "_processed"),
+                "index": self.date_col,
+                "columns": self.code_col,
+            }
+        )
         if begin is not None:
             kwargs.update({f"{self.date_col}__ge": begin})
         if end is not None:
             kwargs.update({f"{self.date_col}__le": end})
-        df = ParquetManager.read(self, **kwargs)
+        df = pm.read(**kwargs)
 
         return df.sort_index().dropna(axis=0, how="all")
+
+    def save(
+        self,
+        name: str,
+        df: pd.DataFrame,
+        processors: list[callable] = None,
+        partition_col: str = "month",
+        partitioner: str = "month",
+    ):
+        pm = ParquetManager(
+            self.path / name,
+            index_col=[self.time_col, self.code_col],
+            partition_col=partition_col,
+        )
+        if df.index.nlevels == 1:
+            for processor in processors or [zscore, partial(madoutlier, dev=5)]:
+                processed = processor(df)
+            df = pd.concat(
+                [processed.stack(), df.stack().to_frame("_processed")], axis=1
+            ).reset_index(names=["time", "code"])
+            pm.upsert(factor, partitioner=partitioner)
+
+        elif df.index.nlevels == 2:
+            factors = [df[col].unstack() for col in df.columns]
+            for processor in processors:
+                factors = [processor(factor) for factor in factors]
+            factor = pd.concat(
+                [factor.stack() for factor in factors],
+                keys=df.columns.str + "_processed",
+                axis=1,
+            )
+            factor = pd.concat([factor, df], axis=1).reset_index(names=["time", "code"])
+            pm.update_insert(factor, partitioner=partitioner)
 
 
 class DuckDBFactorSource(FactorSource):
@@ -180,3 +214,8 @@ class DuckDBFactorSource(FactorSource):
             ).fetch_df()
             data = data.pivot(index="time", columns="code", values=value)
         return data
+
+    def save(self, name: str, df: pd.DataFrame, processors: list[callable] = None):
+        raise NotImplementedError(
+            "Save method for DuckDBFactorSource is not implemented"
+        )
