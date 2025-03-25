9c5e1df14b6a6241ef70c0c71cd8e1b0ddcd3a6a
ppoak <ppoak@foxmail.com>
2025-03-25 15:01:25 +0800

    修复CapitalFlow类因子

--- contrib/capflow.py
+++ contrib/capflow.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from .base import FactorManager
+from .factor import BaseFactor
 
 
-class CapFlowFactor(FactorManager):
+class CapitalFlow(BaseFactor):
 
-    def get_stock_connect_stableinc(self, date: pd.Timestamp) -> pd.Series:
-        rollback = quotes_day.get_trading_days_rollback(date, 20)
-        chold = stock_connect.read("shares_holding", start=rollback, stop=date)
-        shares = quotes_day.read("circulation_a", start=rollback, stop=date)
+    def get_stock_connect_stableinc(self, time: pd.Timestamp) -> pd.Series:
+        rollback = self.source.get_time(time, 20)[0]
+        chold = self.source.get_factor("shares_holding", begin=rollback, end=time)
+        shares = self.source.get_factor("circulation_a", begin=rollback, end=time)
         per = chold / shares
         res = (per.iloc[-1] - per.iloc[0]) / per.std()
-        res.name = date
         return res
