51e302dd3cb1859067962c25fcb3e342152c266c
ppoak <ppoak@foxmail.com>
2025-03-25 15:02:03 +0800

    恢复DerivativePrice类因子和MomentumReverse类因子

---
+++ contrib/derivative_price.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from .factor import BaseFactor
+
+
+class DerivativePrice(BaseFactor):
+
+    def calc_weighted_price(self, time: pd.Timestamp):
+        price = self.source.get_factor(
+            name="close", begin=time, end=time + pd.offsets.Hour(n=16)
+        )
+        volume = self.source.get_factor(
+            name="volume", begin=time, end=time + pd.offsets.Hour(n=16)
+        )
+        volume_weighted = (price * volume / volume.sum()).sum()
+        time_weighted = price.mean()
+        tail_weighted = (
+            price.between_time("14:30", "15:00")
+            * volume.between_time("14:30", "15:00")
+            / volume.between_time("14:30", "15:00").sum()
+        ).sum()
+        head_weighted = (
+            price.between_time("09:30", "10:00")
+            * volume.between_time("09:30", "10:00")
+            / volume.between_time("09:30", "10:00").sum()
+        ).sum()
+        return pd.concat(
+            [volume_weighted, time_weighted, tail_weighted, head_weighted],
+            axis=1,
+            keys=["volume_weighted", "time_weighted", "tail_weighted", "head_weighted"],
+        )
