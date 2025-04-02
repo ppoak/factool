0907f068ccccc9a6f8e7f9ea89155740d8aa8813
ppoak <ppoak@foxmail.com>
2025-04-02 22:17:39 +0800

    修复MarketSize因子的问题

--- contrib/market_size.py
+++ contrib/market_size.py
@@ -8,9 +8,8 @@ class MarketSize(BaseFactor):
 
     def calc_market_sizes(self, time: str | pd.Timestamp) -> pd.Series:
         shares = self.source.get_factor("circulation_a", begin=time, end=time)
-        price = self.source.get_factor("close", begin=time, end=time)
-        adjfactor = self.source.get_factor("adjfactor", begin=time, end=time)
-        log = np.log(shares * price * adjfactor).squeeze()
+        price = self.source.get_factor("close_post", begin=time, end=time)
+        log = np.log(shares * price).squeeze()
         model = sm.OLS((log**3).dropna(), sm.add_constant(log).dropna()).fit()
         nonlinear = model.resid
         return pd.concat(
