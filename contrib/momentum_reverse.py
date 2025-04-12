4c0906ecd4581bd91819b7f9ff36d2c32bcf9ab9
ppoak <ppoak@foxmail.com>
2025-04-13 01:21:56 +0800

    添加decomposed_momentum以你这

--- contrib/momentum_reverse.py
+++ contrib/momentum_reverse.py
@@ -4,9 +4,9 @@ from .factor import BaseFactor
 
 class MomentumReverse(BaseFactor):
 
-    def calc_naive_return_momentum(self, date: str | pd.Timestamp) -> pd.DataFrame:
-        rollback = self.source.get_time(date, 252)[0]
-        price = self.source.get_factor("close", rollback, date)
+    def calc_naive_return_momentum(self, time: str | pd.Timestamp) -> pd.DataFrame:
+        rollback = self.source.get_time(time, 252)[0]
+        price = self.source.get_factor("close", rollback, time)
         return pd.concat(
             [
                 price.iloc[-1] / price.iloc[-5] - 1,
@@ -23,9 +23,9 @@ class MomentumReverse(BaseFactor):
             ],
         )
 
-    def calc_nonrecent_return_momentum(self, date: str | pd.Timestamp) -> pd.DataFrame:
-        rollback = self.source.get_time(date, 252)[0]
-        price = self.source.get_factor("close", rollback, date).iloc[:-5]
+    def calc_nonrecent_return_momentum(self, time: str | pd.Timestamp) -> pd.DataFrame:
+        rollback = self.source.get_time(time, 252)[0]
+        price = self.source.get_factor("close", rollback, time).iloc[:-5]
         return pd.concat(
             [
                 price.iloc[-1] / price.iloc[-5] - 1,
@@ -39,5 +39,45 @@ class MomentumReverse(BaseFactor):
                 "nonrecent_monthly_return",
                 "nonrecent_quarterly_return",
                 "nonrecent_yearly_return",
+            ],
+        )
+
+    def calc_decomposed_momentum(self, time: str | pd.Timestamp) -> pd.DataFrame:
+        rollback = self.source.get_time(time, 1)[0]
+        close_m = self.sources[1].get_factor(
+            "close_post", time, time + pd.offsets.Hour(16)
+        )
+        close_d = self.source.get_factor("close_post", rollback, time)
+        return_d = (close_d / close_d.shift(1) - 1).iloc[-1]
+        return_m = close_m / close_m.shift(1) - 1
+        return_m_mean = return_m.mean()
+        return_m_std = return_m.std()
+        return_m_skew = return_m.skew()
+        return_m_kurt = return_m.kurt()
+        return_m_abnormal = return_m.where(
+            return_m - return_m_mean > 2 * return_m_std
+        ).mean()
+        return_m_normal = return_m.where(
+            return_m - return_m_mean <= 2 * return_m_std
+        ).mean()
+        return pd.concat(
+            [
+                return_d,
+                return_m_mean,
+                return_m_std,
+                return_m_skew,
+                return_m_kurt,
+                return_m_abnormal,
+                return_m_normal,
+            ],
+            axis=1,
+            keys=[
+                "interday_return",
+                "intraday_return",
+                "intraday_return_std",
+                "intraday_return_skew",
+                "intraday_return_kurt",
+                "intraday_return_abnormal",
+                "intraday_return_normal",
             ],
         )
