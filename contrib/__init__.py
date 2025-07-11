b5113e2f90c81456fb75aa8b99ba03a6284b1eac
ppoak <ppoak@foxmail.com>
2025-07-11 23:29:23 +0800

    修复market_size因子问题

--- contrib/__init__.py
+++ contrib/__init__.py
@@ -2,6 +2,6 @@ from .derivative_price import DerivativePrice
 from .momentum_reverse import MomentumReverse
 from .return_distribution import ReturnDistribution
 from .capital_flow import CapitalFlow
-from .market_size import MarketSize
+from .market_size import calc_market_sizes
 from .liquidity import Liquidity
 from .price_volume import PriceVolumeDay
