25c7bc8a3788341caae0fdadda0e0fe62446fabb
ppoak <ppoak@foxmail.com>
2025-09-20 22:04:25 +0800

    更换backtest底层数据接口

--- script/backtest.py
+++ script/backtest.py
@@ -1,29 +1,21 @@
-# %% [markdown]
-# 因子测试
 import factool
 import numpy as np
 import pandas as pd
-from quool import ParquetManager, setup_logger
+from parquool import DuckParquet, setup_logger
 
 
-# %% [markdown]
-# 参数设定
-factor_name = "corr_processed"  # The factor you want to analyze
-factor_path = (
-    "data/compound_volume"  # Path to the data folder (default is "data/price_volume")
-)
-price_path = "D:/Documents/DataBase/quotes_day"  # Path to the data folder (default is "data/price_volume")
-benchmark_path = "D:/Documents/DataBase/index_quotes_day"
+factor_name = "log_market_size"  # The factor you want to analyze
+factor_path = "D:/Documents/DataSet/factor_lab/market_size"  # Path to the data folder (default is "data/price_volume")
+price_path = "D:/Documents/DataSet/quotes_day"  # Path to the data folder (default is "data/price_volume")
+benchmark_path = "D:/Documents/DataSet/index_quotes_day"
 log_path = (
     f"out/{factor_name}.log"  # Path to the log file (default is "out/factor_name.log")
 )
 begin = "2015-01-01"  # Start date
 end = "now"  # End date
-ptype = (
-    "open_post"  # Price type to use for backtesting (e.g., "open", "close", "high", "low")
-)
-benchmark_code = "000985.XSHG"
-time_col = "time"  # Name of the time column in the data
+ptype = "open_post"  # Price type to use for backtesting (e.g., "open", "close", "high", "low")
+benchmark_code = "000985.SH"
+time_col = "date"  # Name of the time column in the data
 code_col = "code"  # Name of the code column in the data
 freq = 5  # Frequency of rebalance (e.g., 1 for daily, 5 for weekly)
 weight = None  # Portfolio weights (default is None)
@@ -34,56 +26,47 @@ commission = 0.0005  # Commission rate for trading
 out_path = f"out/report_{factor_name}_{ptype}_{benchmark_code}_{freq}_{topk}.png"  # Output file path for the report
 logger = setup_logger("factor_test", file=log_path, level="INFO")
 
-# %% [markdown]
-# 价格数据读取
-price_source = ParquetManager(price_path)
-price_data = {}
-price = price_source.read(
-    index=time_col,
-    columns=code_col,
-    pivot=ptype,
-    **{f"{time_col}__ge": begin, f"{time_col}__le": end},
+price_source = factool.DuckParquetSource(
+    price_path, time_col=time_col, code_col=code_col
 )
-for field in ["st", "suspended", "limit_up", "limit_down", "high_post", "low_post"]:
-    price_data[field] = price_source.read(
-        index=time_col,
-        columns=code_col,
-        pivot=field,
-        **{f"{time_col}__ge": begin, f"{time_col}__le": end},
+price_data = {}
+price = price_source.get_factor(
+    name=ptype,
+    begin=begin,
+    end=end,
+)
+for field in ["st", "suspended", "limit_up", "limit_down", "high", "low"]:
+    price_data[field] = price_source.get_factor(
+        name=field,
+        begin=begin,
+        end=end,
     )
-# feasible = (
-#     ~(price_data["st"].replace(np.nan, False))
-#     & ~(price_data["suspended"].replace(np.nan, False))
-#     & (price_data["low"] < price_data["limit_up"])
-#     & (price_data["high"] > price_data["limit_down"])
-# )
-feasible = None
+feasible = (
+    ~(price_data["st"].replace(np.nan, False))
+    & ~(price_data["suspended"].replace(np.nan, False))
+    & (price_data["low"] < price_data["limit_up"])
+    & (price_data["high"] > price_data["limit_down"])
+)
 if benchmark_code is not None:
     benchmark = (
-        ParquetManager(benchmark_path)
-        .read(
-            index=time_col,
-            columns="close",
-            **{
-                f"{code_col}": benchmark_code,
-                f"{time_col}__ge": begin,
-                f"{time_col}__le": end,
-            },
+        DuckParquet(benchmark_path)
+        .select(
+            columns=[time_col, "close"],
+            where=f"{code_col} = ? AND {time_col} >= ? AND {time_col} <= ?",
+            params=[benchmark_code, begin, end],
+            order_by=time_col,
         )
+        .set_index(time_col)
         .squeeze()
     )
 else:
     benchmark = None
 
-# %% [markdown]
-# 因子数据读取
-factor_source = factool.ParquetFactorSource(
-    factor_path, time_col=time_col, code_col=code_col
+factor_source = factool.DuckParquetSource(
+    factor_path, time_col="date", code_col=code_col
 )
 factor_data = factor_source.get_factor(factor_name, begin=begin, end=end)
 
-# %% [markdown]
-# 因子回测
 evaluator = factool.Evaluator(
     factor=factor_data,
     price=price,
@@ -100,10 +83,6 @@ evaluator(
     commission=commission,
 )
 
-# %% [markdown]
-# 因子报告
-# 因子报告提供一表三图，有topk+ngroup、ic的测试结果
-# 分层测试可能失败，关注CRITICAL信息
 pd.concat([evaluator.ic, evaluator.ic.cumsum()], axis=1, keys=["raw", "cumsum"]).plot(
     secondary_y="cumsum"
 )
